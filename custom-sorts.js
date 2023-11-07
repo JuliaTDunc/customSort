896413315500b34483f959d3184a4753346cc871
Julia Duncan <juliataylorduncan@gmail.com>
2023-11-06 21:42:28 -0800

    reverseBase

--- custom-sorts.js
+++ custom-sorts.js
@@ -1,22 +1,42 @@
 function ageSort(users) {
-  // Your code here 
+  return users.sort((a,b) => a.age - b.age)
 }
 
 function oddEvenSort(arr) {
-  // Your code here 
+  return arr.sort((a,b) => {
+    if(a % 2 === 1 && b % 2 === 0) return -1
+    if(a % 2 === 0 && b % 2 === 1) return 1 
+    return a - b;
+ })
 }
 
 function validAnagrams(s, t) {
-  // Your code here 
+ return  (s.split('').sort().join() === t.split('').sort().join())
 }
 
 function reverseBaseSort(arr) {
-  // Your code here 
+  return arr.sort((a, b) => {
+    let logA = Math.floor(Math.log10(a))
+    let logB = Math.floor(Math.log10(b))
+    if(logA === logB) {
+      return a - b;
+    }
+    return b - a;
+    
+  })
 }
 
 function frequencySort(arr) {
-  // Your code here 
+  return arr.sort((a, b) => {
+    if(b - a > 0) return 1;
+    if(b - a < 0) return -1;
+    return a - b;
+  })
+
+
 }
+//1(yes swap)
+//-1(same)
 
 module.exports = [
   oddEvenSort,
